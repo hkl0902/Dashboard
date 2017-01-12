d79e6f7c924959208e48008edaca71d261fd5ff3
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-12 12:15:55 -0800

    Initial Commit

---
+++ Dashboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dashboard
+//
+//  Created by Hyunkyu Lee on 1/12/17.
+//  Copyright © 2017 Hyunkyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

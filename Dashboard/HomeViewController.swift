edcd02a1a64a315eb627f9d44b4c5108a05848ba
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-12 14:11:19 -0800

    Creates a custom cell to show in the HomeView

--- Dashboard/HomeViewController.swift
+++ Dashboard/HomeViewController.swift
@@ -17,7 +17,7 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
         self.collectionView?.translatesAutoresizingMaskIntoConstraints = false
         
         // Register Reuse Identifier
-        self.collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "TrackedItemQuickViewCell")
+        self.collectionView?.register(TrackedItemCell.self, forCellWithReuseIdentifier: "TrackedItemQuickViewCell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,11 +31,21 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = self.collectionView?.dequeueReusableCell(withReuseIdentifier: "TrackedItemQuickViewCell", for: indexPath)
-        cell?.backgroundColor = UIColor.red
         return cell!
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.view.frame.width * 0.4, height: 150)
+    }
+}
+
+class TrackedItemCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }

15dc7734cd5ea82b98d5e5d30f8509ae433539ac
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-12 20:02:07 -0800

    Changes model of HomeViewC from the data object to an 'arrayized' version of its attributes

--- Dashboard/HomeViewController.swift
+++ Dashboard/HomeViewController.swift
@@ -39,8 +39,15 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = self.collectionView?.dequeueReusableCell(withReuseIdentifier: "TrackedItemQuickViewCell", for: indexPath)
-        return cell!
+        let cell = self.collectionView?.dequeueReusableCell(withReuseIdentifier: "TrackedItemQuickViewCell", for: indexPath) as! TrackedItemCell
+        cell.title = trackedItems?[indexPath.row].name
+        if cell.options == nil {
+            cell.options = trackedItems?[indexPath.row].answerChoices?.optionsToArray()
+        }
+        if cell.counts == nil {
+            cell.counts = trackedItems?[indexPath.row].answerChoices?.countsToArray()
+        }
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -54,9 +61,33 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
 
 class TrackedItemCell: UICollectionViewCell {
     
+    var title: String? {
+        didSet {
+            titleLabel?.text = title
+            titleLabel?.textAlignment = .center
+            titleLabel?.preferredMaxLayoutWidth = NSAttributedString(string: title!).size().width
+            titleLabel?.backgroundColor = UIColor(red: 0.235, green: 0.541, blue: 0.776, alpha: 1.00)
+        }
+    }
+    
+    var options: [String]?
+    var counts: [Int]?
+    
+    var titleLabel: UILabel?
+    
+    var optionsTableViewController: HomeOptionsTableViewController?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor(red: 0.082, green: 0.537, blue: 0.788, alpha: 1.00)
+        titleLabel = UILabel(frame: CGRect(x: 8, y: 8, width: self.frame.width * 0.33, height: self.frame.height - 16))
+        titleLabel?.lineBreakMode = .byWordWrapping
+        titleLabel?.numberOfLines = 50
+        
+        optionsTableViewController = HomeOptionsTableViewController()
+        addSubview(titleLabel!)
+        addSubview(optionsTableViewController!.tableView)
+
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- Dashboard/Options+CoreDataProperties.swift
+++ Dashboard/Options+CoreDataProperties.swift
@@ -16,6 +16,36 @@ extension Options {
         return NSFetchRequest<Options>(entityName: "Options");
     }
 
+    public func optionsToArray() -> [String] {
+        var arr = [String]()
+        if option_1 != nil {
+            arr.append(option_1!)
+        }
+        if option_2 != nil {
+            arr.append(option_2!)
+        }
+        if option_3 != nil {
+            arr.append(option_3!)
+        }
+        if option_4 != nil {
+            arr.append(option_4!)
+        }
+        if option_5 != nil {
+            arr.append(option_5!)
+        }
+        return arr
+    }
+
+    public func countsToArray() -> [Int] {
+        var arr = [Int]()
+        arr.append(Int(count_1))
+        arr.append(Int(count_2))
+        arr.append(Int(count_3))
+        arr.append(Int(count_4))
+        arr.append(Int(count_5))
+        return arr
+    }
+
     @NSManaged public var option_1: String?
     @NSManaged public var option_2: String?
     @NSManaged public var option_3: String?
